d81855f0b46fa3b1d8ff5f75719dca819f9b8978
Gene Campbell III <gcampbell391@gmail.com>
2020-01-28 22:55:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,10 +18,10 @@ function whileLoop(n){
 }
 
 function  doWhileLoop(num){
-  var i = 0;
+  var i = 0
   function incrementVariable() {
-    i = i + 1;
-    return i;
+    i = i + 1
+    return i
   }
   do{
     console.log('I run once regardless')
