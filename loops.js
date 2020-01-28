acd53a13896adfa17b8b21755c88a31c6b9b720c
Gene Campbell III <gcampbell391@gmail.com>
2020-01-28 22:52:18 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    if(i === 24){
+    array.push(`I am ${i} strange loops.`)  
+    }
+    else{
+    array.push(`I am ${i} strange loop.`)
+  }
+  }
+  return array
+}
+
+function whileLoop(n){
+  while(n>0){
+    console.log(--n)
+  }
+  return 'done'
+}
+
+function  doWhileLoop(num){
+  do{
+    console.log('I run once regardless')
+  }
+  while(num === 0)
+}
